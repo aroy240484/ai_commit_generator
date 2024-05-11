3f0f55c75829e0362b74ca43b46b3350a141fd56
Abhijit Roy <abhijit.roy.2404@gmail.com>
2024-05-10 22:10:11 -0400

    Add executable permissions to commit helper scripts
    - Add executable permissions to commit_message_generator.py script
    - Remove extra newline from setup.sh
    
    Add executable permissions to the commit helper scripts so they can be executed from any directory after install. Also remove extra newline from setup.sh.

--- setup.sh
+++ setup.sh
@@ -10,6 +10,5 @@ python3.11 -m pip install -r requirements.txt
 
 sudo cp commit /usr/local/bin
 sudo cp commit_message_generator.py /usr/local/bin
-
 sudo chmod +x /usr/local/bin/commit
 sudo chmod +x /usr/local/bin/commit_message_generator.py
